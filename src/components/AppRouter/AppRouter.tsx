31a1978298616366d183ffb057a2539f4a811303
Belakhvostau Aliaksandr <aliaksandr@wsa-089-23b.local>
2022-05-17 17:38:55 +0300

    added replace to route

--- src/components/AppRouter/AppRouter.tsx
+++ src/components/AppRouter/AppRouter.tsx
@@ -12,14 +12,14 @@ export const AppRouter: React.FC = () => {
       {privateRoutes.map((route) => (
         <Route key={route.path} path={route.path} element={<route.element />} />
       ))}
-      <Route path="/*" element={<Navigate to={pathesEnum.USERS} />} />
+      <Route path="/*" element={<Navigate to={pathesEnum.USERS} replace />} />
     </Routes>
   ) : (
     <Routes>
       {publicRoutes.map((route) => (
         <Route key={route.path} path={route.path} element={<route.element />} />
       ))}
-      <Route path="/*" element={<Navigate to={pathesEnum.LOGIN} />} />
+      <Route path="/*" element={<Navigate to={pathesEnum.LOGIN} replace />} />
     </Routes>
   );
 };
